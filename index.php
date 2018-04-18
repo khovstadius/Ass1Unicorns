05e811ef4450a639aee08f303eae438887fe00d6
Kalle <khovstadius@hotmail.com>
2018-04-18 21:49:13 +0200

    cleaned the code

--- index.php
+++ index.php
@@ -16,9 +16,6 @@ $res = $client->request('GET', 'http://unicorns.idioti.se/' . $_GET['id'], [
                          'headers' => [
                          'Accept'     => 'application/json' ] ]);
 	
-		
-
-
 $data = json_decode($res->getBody(), true);
 ?>
 
@@ -33,33 +30,33 @@ $data = json_decode($res->getBody(), true);
     </head>
     
     <body>
-		<div class="container">
-		<h1>Enhörningar</h1>
+      <div class="container">
+	    <h1>Enhörningar</h1>
 		<hr>
 		<h4>Id på en noshörning</h4>
 		<form action="index.php" method="get">
-			<div class="form-group">                  
-                    <input type="id" id="id" name="id" class="form-control">
-            </div>
-            <div class="form-group">
-                    <input type="submit" value="Visa enhörning" class="btn btn-success">
-                    <a href="index.php" class="btn btn-primary">Visa alla enhörningar</a>
-			</div>
-			<hr>
-				<p>
-                    <?php
+		  <div class="form-group">                  
+            <input type="id" id="id" name="id" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="submit" value="Visa enhörning" class="btn btn-success">
+            <a href="index.php" class="btn btn-primary">Visa alla enhörningar</a>
+		  </div>
+		    <hr>
+		      <p>
+              <?php
                     
-                    if (isset($_GET['id']) && $_GET['id'] != NULL)
-                    {
+                if (isset($_GET['id']) && $_GET['id'] != NULL)
+                   {
 						 
-						$res = $client->request('GET', 'http://unicorns.idioti.se/' . $_GET['id'], [
-                        'headers' => [
-                        'Accept'     => 'application/json' ] ]);
+				     $res = $client->request('GET', 'http://unicorns.idioti.se/' . $_GET['id'], [
+                     'headers' => [
+                     'Accept'     => 'application/json' ] ]);
                          
-						$data = json_decode($res->getBody(), true);
-						$log->info("Requested info about: " . $data['name']);
-						$image = file_get_contents('http://unicorns.idioti.se/' . $_GET['id']);
-						echo $image;
+					  $data = json_decode($res->getBody(), true);
+				      $log->info("Requested info about: " . $data['name']);
+					  $image = file_get_contents('http://unicorns.idioti.se/' . $_GET['id']);
+					  echo $image;
 					}
                     else
                     {
@@ -69,23 +66,22 @@ $data = json_decode($res->getBody(), true);
                     <hr>
                     <p>
 					<?php
-						for ($i=0; $i < count($data) - 5; $i++) 
+					for ($i=0; $i < count($data) - 5; $i++) 
 						{
-							echo "<h5>" . ($data[$i]['id']) . ": " . ($data[$i]['name']) .
-                                 "<a href='index.php?id=".$data[$i]['id'].
-                                 "' class='btn btn-default' style='float:right;'>" . 
-                                 "Mer Information" . "</a>" .
-							     "</h5>" . 
-								 "<hr>";
-								 
-								  
+						echo "<h5>" . ($data[$i]['id']) . ": " . ($data[$i]['name']) .
+                             "<a href='index.php?id=".$data[$i]['id'].
+                             "' class='btn btn-default' style='float:right;'>" . 
+                             "Mer Information" . "</a>" .
+			     		     "</h5>" . 
+							 "<hr>";
+							  
 						}
 						$log->info("Requested info about all unicorns");
 					}
 					?>
 					</p>
                     
-            </div>
+          </div>
         </form>
        </div>
     </body>
